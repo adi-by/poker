123bd28433836cb949a97c90d40c5bcc04e2d797
adi <adi.benyoash@gmail.com>
2013-09-21 12:50:21 +0300

    Passing game again, and fixed remove bug

--- game/views.py
+++ game/views.py
@@ -1,12 +1,11 @@
 from django.shortcuts import render, get_object_or_404
-from models import Game
+from models import Game, Player
 
 def get_game_context(request, game):
     """
     Returns the context we want for the client for a given game.
     """
-    context = {'blinds': game.blind_schema.blind_set.all().order_by('level')}
-    return render(request, 'game/game.html', context)
+    return render(request, 'game/game.html', {'game': game})
 
 def game(request, game_id):
     """
@@ -21,6 +20,7 @@ def remove(request, game_id, player_id):
     """
     
     curr_game = get_object_or_404(Game, pk=game_id)
-    curr_game.remove_player(player_id)
+    curr_player = get_object_or_404(Player, pk=player_id)
+    curr_game.remove_player(curr_player)
     
     return get_game_context(request, curr_game)
