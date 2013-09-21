30610f29b81fe523c985ccdf1e9ed9b581831cd9
Sion Schori <sion.schori@gmail.com>
2013-09-21 13:17:32 +0300

    Make antes visible when needed.

--- static/js/blind.js
+++ static/js/blind.js
@@ -1,31 +1,38 @@
-function Blind($scope, $rootScope, blinds) {
-    $scope.update_level = function(level) {
-        var level_blinds = blinds[level - 1].fields;
-        $scope.small = level_blinds.small;
-        $scope.big = level_blinds.big;
+function BlindValue($scope, $rootScope, $element, blinds) {
+    $scope.init = function(name, is_next) {
+        $scope.name = name;
+        $scope.is_next = is_next;
+        $scope.update_level(1); // TODO: get from server.
     }
-    $scope.update_level(1); // TODO: get from server.
+
+    $scope.update_level = function(level) {
+        if (!$scope.is_next) {
+            $scope.value = blinds[level - 1].fields[$scope.name];
+        } else {
+            if (level < blinds.length) {
+                $scope.value = blinds[level].fields[$scope.name];
+                $element.show();
+            } else {
+                $scope.value = null;
+                $element.hide();
+            }
+        }
+    }
+
     $rootScope.$on('levelUp', function(_, level) {
         $scope.update_level(level);
     });
 }
 
-function Ante($scope, $rootScope, $element, blinds) {
-    $scope.set_ante = function(ante) {
-        $scope.ante = ante;
-        if (ante > 0) {
-            $element.show();
+function AnteValue($scope, $rootScope, $element, blinds) {
+    BlindValue($scope, $rootScope, $element, blinds);
+    $scope._super_update_level = $scope.update_level;
+    $scope.update_level = function (level) {
+        $scope._super_update_level(level);
+        if ($scope.value == 0) {
+            $element.parent().hide();
         } else {
-            $element.hide();
+            $element.parent().show();
         }
     }
-
-    $scope.update_level = function(level) {
-        $scope.set_ante(blinds[level - 1].fields.ante);
-    }
-    $scope.update_level(1); // TODO: get from server.
-
-    $rootScope.$on('levelUp', function(_, level) {
-        $scope.update_level(level);
-    });
 }
