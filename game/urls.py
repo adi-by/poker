5fd04d84d2f60c63a1a8aea233cbc1a47da7a3dc
adi <adi.benyoash@gmail.com>
2013-09-24 20:00:52 +0300

    Getting clock action in GET, not URL

--- game/urls.py
+++ game/urls.py
@@ -5,7 +5,7 @@ urlpatterns = patterns('',
     url(r'^(?P<game_id>\d+)/$', views.game, name='game'),
     url(r'^(?P<game_id>\d+)/remove/(?P<player_id>\d+)/$', views.remove, 
         name='remove'),
-    url(r'^(?P<game_id>\d+)/clock/(?P<is_running>\d+)/$', views.clock, 
+    url(r'^(?P<game_id>\d+)/clock/$', views.clock, 
         name='clock'),
     url(r'^(?P<game_id>\d+)/get_time/$', views.get_time, name='get_time')
 )
