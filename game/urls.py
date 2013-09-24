7a7b3ebfbb4300db85c79adc389e752d72f59e28
adi <adi.benyoash@gmail.com>
2013-09-24 19:37:44 +0300

    Adding views for time stuff

--- game/urls.py
+++ game/urls.py
@@ -4,5 +4,8 @@ import views
 urlpatterns = patterns('',
     url(r'^(?P<game_id>\d+)/$', views.game, name='game'),
     url(r'^(?P<game_id>\d+)/remove/(?P<player_id>\d+)/$', views.remove, 
-        name='remove')
+        name='remove'),
+    url(r'^(?P<game_id>\d+)/start/$', views.start, name='start'),
+    url(r'^(?P<game_id>\d+)/stop/$', views.stop, name='stop')
+    url(r'^(?P<game_id>\d+)/get_time/$', views.get_time, name='get_time')
 )
