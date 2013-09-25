d499c0e69ec3673e0a1b61896daafc00d4b68c0d
adi <adi.benyoash@gmail.com>
2013-09-25 13:14:39 +0300

    Dynamic urling

--- game/urls.py
+++ game/urls.py
@@ -2,6 +2,10 @@ from django.conf.urls import patterns, url
 from django_sse.redisqueue import RedisQueueView
 import views
 
+class DynamicRedisQueueView(RedisQueueView):
+    def get_redis_channel(self):
+        return self.kwargs['game_id'] or self.redis_channel
+    
 urlpatterns = patterns('',
     url(r'^(?P<game_id>\d+)/$', views.game, name='game'),
     url(r'^(?P<game_id>\d+)/remove/(?P<player_id>\d+)/$', views.remove, 
@@ -9,6 +13,7 @@ urlpatterns = patterns('',
     url(r'^(?P<game_id>\d+)/clock/$', views.clock, 
         name='clock'),
     url(r'^(?P<game_id>\d+)/get_time/$', views.get_time, name='get_time'),
-    url(r'^data_stream/$', RedisQueueView.as_view(redis_channel="data"), 
+    url(r'^(?P<game_id>\d+)data_stream/$', 
+        DynamicRedisQueueView.as_view(redis_channel="data"), 
         name="data_stream"),
 )
