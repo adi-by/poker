0e29033424aad6d794fcfe984d65e1550ec52f97
Sion Schori <sion.schori@gmail.com>
2013-09-26 13:04:17 +0300

    making player selection more user friendly.

--- static/js/players_select.js
+++ static/js/players_select.js
@@ -1,12 +1,27 @@
 function PlayersSelect($scope) {
-    $scope.players = [{name: 'Sion', key: 1},
-        {name: 'Adi', key:2},
-        {name: 'Lior', key:3}];
+    $scope.players = [{name: 'Sion', key: 1, playing: false},
+        {name: 'Adi', key:2, playing: false},
+        {name: 'Lior', key:3, playing: false}];
+
+    $scope.flip = function(player) {
+        player.playing = !player.playing;
+    }
+
+    $scope.row_class = function(player) {
+        if (player.playing) {
+            return "selected-element";
+        } else {
+            return "";
+        }
+    }
 }
 
 function AddPlayer($scope, $state) {
+    var key = 10;
+
     $scope.save = function() {
-        console.log('Added player ' + $scope.name);
+        $scope.players.push({name: $scope.name, key: key});
+        key++;
         $state.go('start');
     }
 }
