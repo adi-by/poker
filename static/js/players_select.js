e1867a61f6d83275bf70b72dbcaf18bd93b7737c
Sion Schori <sion.schori@gmail.com>
2013-10-04 17:03:54 +0300

    Making add player update players view.

--- static/js/players_select.js
+++ static/js/players_select.js
@@ -1,7 +1,10 @@
 function PlayersSelect($scope, $rootScope) {
-    $scope.players = [{name: 'Sion', key: 1, playing: false},
-        {name: 'Adi', key:2, playing: false},
-        {name: 'Lior', key:3, playing: false}];
+    if (!('players' in $rootScope)) {
+        $rootScope.players = [{name: 'Sion', key: 1, playing: false},
+            {name: 'Adi', key:2, playing: false},
+            {name: 'Lior', key:3, playing: false}];
+    }
+    $scope.players = $rootScope.players;
 
     $scope.flip = function(player) {
         player.playing = !player.playing;
@@ -18,10 +21,16 @@ function PlayersSelect($scope, $rootScope) {
 
 function AddPlayer($scope, $rootScope, $state) {
     var key = 10;
+    if ('curr_key' in $rootScope) {
+        key = $rootScope.curr_key;
+    }
 
     $scope.save = function() {
-        console.log('New player ' + $scope.name);
+        var player = {name: $scope.name, key: key, playing: false};
+        console.log('New player', player);
         key++;
+        $rootScope.curr_key = key;
+        $rootScope.players.push(player);
         $state.go('start');
     }
 }
